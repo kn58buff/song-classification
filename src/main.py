0171c0e69f2522a7c892d350581b89a8ab384931
Kevin Nguyen <kn58@buffalo.edu>
2024-06-04 11:30:00 -0400

    Requests

--- src/main.py
+++ src/main.py
@@ -1,7 +1,26 @@
 import requests
 import os
 import dotenv
+import webbrowser
+from urllib.parse import urlencode
 
 
 dotenv.load_dotenv(dotenv_path="src/variables.env")
-print(os.getenv("PRIVATE_KEY"))
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+
+auth_headers = {"response_type" : "code",
+                "client_id" : CLIENT_ID,
+                "redirect_uri": "http://localhost:8080/callback",
+                "scope" : "playlist-read-private"}
+
+webbrowser.open("https://accounts.spotify.com/authorize?" + urlencode(auth_headers))
+
+"""response = requests.post(url="https://accounts.spotify.com/api/token",
+              headers={"Content-Type" : "application/x-www-form-urlencoded"},
+              data= {"grant_type" : "client_credentials",
+                     "client_id" : CLIENT_ID,
+                     "client_secret" : CLIENT_SECRET}) if "response" not in locals() else None
+
+access_token = response.json()["access_token"]
+print(access_token)"""
